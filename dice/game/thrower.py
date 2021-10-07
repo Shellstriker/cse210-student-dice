5c7e979d9dd72df8e7a2d14508b392c54de052fd
Shellstriker <91568697+Shellstriker@users.noreply.github.com>
2021-10-06 18:02:27 -0600

    Project

--- dice/game/thrower.py
+++ dice/game/thrower.py
@@ -1,3 +1,22 @@
 import random
 
+from dice.game.director import Director
+
 # TODO: Define the Thrower class here.
+class thrower(dice, num_throws):    
+    def __init__(self):
+        self.dice = []
+        self.num_throws =0
+        
+    def can_throw(self):
+        return (self.dice.count(5) > 0 or self.dice.count(1) > 0 or self.num_throws == 0)
+    
+    def get_points(self):
+        return self.dice.count(5)*50 + self.dice.count(1)*100
+
+    def throw_dice():
+        self.dice.clear()
+        for i in range(5):
+            result = random.randint(1,6)
+            self.dice.append(result)
+        self.num_throws += 1
